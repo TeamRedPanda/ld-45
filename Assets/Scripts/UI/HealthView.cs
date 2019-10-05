145fb5c61ffe613e5f732ed5d4307a2223168f20
Milena <milenaferreira294@hotmail.com>
2019-10-05 19:44:05 -0300

    Create health view

---
+++ Assets/Scripts/UI/HealthView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthView : MonoBehaviour
+{
+    public Image healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHealthChange( float currentHealth, float maxHealth )
+    {
+        healthBar.fillAmount = currentHealth / maxHealth;
+    }
+}
