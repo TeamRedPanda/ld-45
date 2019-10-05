d8e757260c1f8f9b24badebee7638a923c244703
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-05 18:18:57 -0300

    Add PlayerInput class

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ActorMovement))]
+public class PlayerInput : MonoBehaviour
+{
+    ActorMovement m_ActorMovement;
+
+    void Awake()
+    {
+        m_ActorMovement = GetComponent<ActorMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var direction = new Vector3(horizontal, 0f, vertical);
+        m_ActorMovement.Move(direction, true);
+    }
+}
