08ed532788a6ca6032d207a229473e66a9a8e8f7
Milena <milenaferreira294@hotmail.com>
2019-10-05 18:23:01 -0300

    Add simple health class

---
+++ Assets/Scripts/Actor/ActorHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActorHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( currentHealth <= 0 ) {
+            Destroy( gameObject );
+        }
+    }
+
+    public void ApplyDamage( int damage )
+    {
+        currentHealth -= damage;
+    }
+}
