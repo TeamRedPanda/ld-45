285db6917c7f71832ad70a7ab16c8202f10bb2c4
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-06 13:49:49 -0300

    Add projectiles

---
+++ Assets/Scripts/Projectile/ProjectileDamage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+class ProjectileDamage : MonoBehaviour
+{
+    public int DamageMin;
+    public int DamageMax;
+
+    public float HitKnockback;
+
+    public void ApplyDamage(ActorHealth actor)
+    {
+        int damage = Random.Range(DamageMin, DamageMax);
+        actor.TakeDamage(damage, this.gameObject.transform.position, HitKnockback);
+        Destroy(this.gameObject);
+    }
+}
