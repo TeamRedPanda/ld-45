4d6e5deeb024938671621d31bc4122888022dcda
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-05 21:14:55 -0300

    Create AnimationEventReceiver
    This script is responsible for getting the events from the current animation.

---
+++ Assets/Scripts/Animation/AnimationEventReceiver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEventReceiver : MonoBehaviour
+{
+    [Header("Active phase")]
+    public UnityEvent OnActivePhaseStart;
+    public UnityEvent OnActivePhaseEnd;
+
+    [Header("Movement status")]
+    public UnityEvent OnAttackAnimationStart;
+    public UnityEvent OnBackswingAnimationStart;
+
+    public void ActivePhaseStart()
+    {
+        OnActivePhaseStart.Invoke();
+    }
+
+    public void ActivePhaseEnd()
+    {
+        OnActivePhaseEnd.Invoke();
+    }
+
+    public void AttackAnimationStarted()
+    {
+        OnAttackAnimationStart.Invoke();
+    }
+
+    public void BackswingAnimationStarted()
+    {
+        OnBackswingAnimationStart.Invoke();
+    }
+}
