96d2bc3cb46de8b90e3c898421724b7f3e5c8ef0
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-05 23:22:58 -0300

    Create enemy actor prefab variant with world hp bar

---
+++ Assets/Scripts/UI/CameraFacingBillboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    private Camera m_Camera;
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+
+    void Awake()
+    {
+        m_Camera = Camera.main;
+    }
+}
