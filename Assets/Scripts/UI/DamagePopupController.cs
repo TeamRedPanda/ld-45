4ef408a6fd7cc365af12b09d1c3dc52f037dcdf0
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-06 16:01:37 -0300

    Add damage popup to attacks

---
+++ Assets/Scripts/UI/DamagePopupController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+class DamagePopupController : SingletonBehaviour<DamagePopupController>
+{
+    public FloatingText DamagePopupPrefab;
+
+    public void ShowDamage(Vector3 position, float value, Color color)
+    {
+        Vector2 posInScreen = Camera.main.WorldToScreenPoint(position);
+
+        var floatingText = Instantiate(DamagePopupPrefab, this.transform);
+        floatingText.Text = value.ToString();
+        floatingText.Color = color;
+
+        floatingText.GetComponent<RectTransform>().position = posInScreen;
+    }
+}
