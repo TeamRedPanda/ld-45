f2629c6ba1efedab821bbf9b58dda7e10aa66777
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-07 14:03:20 -0300

    Add death emote when actor dies

---
+++ Assets/Scripts/UI/FloatingIcon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingIcon : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Destroy(this.gameObject, clipInfo[0].clip.length);
+    }
+}
