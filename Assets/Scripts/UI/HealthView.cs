324758ac80a8c7dfd93678a9c68912294183adf1
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-10-12 19:44:55 -0300

    Change color of enemy hp bar based on hp lost

--- Assets/Scripts/UI/HealthView.cs
+++ Assets/Scripts/UI/HealthView.cs
@@ -6,21 +6,26 @@ using UnityEngine.UI;
 public class HealthView : MonoBehaviour
 {
     public Image healthBar;
+    public Gradient BarColor;
+    public Image HealthImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnHealthChange( float currentHealth, float maxHealth )
+    public void OnHealthChange(float currentHealth, float maxHealth)
     {
-        healthBar.fillAmount = currentHealth / maxHealth;
+        float fillAmount = currentHealth / maxHealth;
+        healthBar.fillAmount = fillAmount;
+
+        HealthImage.color = BarColor.Evaluate(fillAmount);
     }
 }
